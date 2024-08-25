cd486382b20dc96ef172e7d5f1da0a0f8f96e62b
Ekaterina771982 <tsoy_777@mail.ru>
2024-08-26 02:46:59 +0300

    edit

---
+++ index.js
@@ -0,0 +1,3 @@
+console.log("Hello from Parcel!")
+
+require(`./server`)
---
+++ server.js
@@ -0,0 +1,5 @@
+const math = require(`./math`)
+
+
+console.log("server!", math.add(2, 3))
+console.log("server!", math.subtract(5, 2))
